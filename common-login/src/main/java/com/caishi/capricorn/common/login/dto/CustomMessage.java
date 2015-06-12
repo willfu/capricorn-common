bb1cd2f3d36989480fc7bbaf559322a0c948b141
李国超 <james.li@9icaishi.net>
2015-06-12 14:20:16 +0800

    属性修改

--- common-login/src/main/java/com/caishi/capricorn/common/login/dto/CustomMessage.java
+++ common-login/src/main/java/com/caishi/capricorn/common/login/dto/CustomMessage.java
@@ -7,16 +7,14 @@ public class CustomMessage extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public CustomMessage(String code,Object data){
+	public CustomMessage(String code){
 		super();
 		this.setCode(code);
-		this.setData(data);
 	}
 	
-	public CustomMessage(String code,Object data,String message){
+	public CustomMessage(String code,String message){
 		super(message);
 		this.setCode(code);
-		this.setData(data);
 	}
 	
 	/**
@@ -24,40 +22,11 @@ public class CustomMessage extends Exception {
 	 */
 	private String code;
 	
-	/**
-	 * 获取异常编码
-	 * @return 异常编码
-	 */
 	public String getCode() {
 		return code;
 	}
 
-	/**
-	 * 设置异常编码
-	 * @param code 异常编码
-	 */
 	public void setCode(String code) {
 		this.code = code;
-	}
-	
-	/**
-	 * 异常结果
-	 */
-	private Object data;
-
-	/**
-	 * 获取异常结果
-	 * @return 异常结果
-	 */
-	public Object getData() {
-		return data;
-	}
-
-	/**
-	 * 设置异常结果
-	 * @param data 异常结果
-	 */
-	public void setData(Object data) {
-		this.data = data;
 	}
 }

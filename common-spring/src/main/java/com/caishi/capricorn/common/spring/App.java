c9657d6c583b478f71daef98a1d264bc8bb9a6f0
yyc <yeyanchao.whu@gmail.com>
2015-06-15 11:52:32 +0800

    add common-spring module

---
+++ common-spring/src/main/java/com/caishi/capricorn/common/spring/App.java
@@ -0,0 +1,13 @@
+package com.caishi.capricorn.common.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

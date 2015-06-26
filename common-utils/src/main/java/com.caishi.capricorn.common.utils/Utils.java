63993ffadb69196a41b3da004628460463a654a0
韩小卫 <xwhan1983@163.com>
2015-06-26 11:09:26 +0800

    Add new util function to parse host:port list to array list

--- common-utils/src/main/java/com.caishi.capricorn.common.utils/Utils.java
+++ common-utils/src/main/java/com.caishi.capricorn.common.utils/Utils.java
@@ -1,6 +1,8 @@
 package com.caishi.capricorn.common.utils;
 
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Generic utility
@@ -24,5 +26,21 @@ public class Utils {
                 return aItems[0];
         } catch (Exception e) {}
         return pid;
+    }
+
+    /**
+     * Convert "host1:port1, host2:port2, host3:port3" list to
+     * "[host1, port1],[host2, port2],[host3, port3]" list
+     * @param hostPortList
+     * @return
+     */
+    public List<String[]> convert2HostPortPairs(List<String> hostPortList) {
+        List<String[]> hostPortPairList = new ArrayList<String[]>();
+
+        for (String hostPort : hostPortList) {
+            hostPortPairList.add(new String[] { hostPort.split(":")[0], hostPort.split(":")[1] });
+        }
+
+        return hostPortPairList;
     }
 }

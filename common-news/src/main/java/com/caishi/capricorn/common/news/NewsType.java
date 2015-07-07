816cb4a6e13082bb8a43d76a913e3e6cf367034f
James.li <James.li@9icaishi.net>
2015-07-07 19:16:05 +0800

    add news information

---
+++ common-news/src/main/java/com/caishi/capricorn/common/news/NewsType.java
@@ -0,0 +1,18 @@
+package com.caishi.capricorn.common.news;
+
+public enum NewsType {
+    /**
+     * 新闻
+     */
+    NEWS,
+
+    /**
+     * 活动
+     */
+    ACTIVITY,
+
+    /**
+     * 广告
+     */
+    ADVERTISE;
+}

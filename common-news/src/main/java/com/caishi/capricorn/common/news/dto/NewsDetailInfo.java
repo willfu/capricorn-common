18b165e8950f9236a75eac3fd1b9148e64575515
James.li <James.li@9icaishi.net>
2015-07-10 14:39:24 +0800

    add new module named as common-social

--- common-news/src/main/java/com/caishi/capricorn/common/news/dto/NewsDetailInfo.java
+++ common-news/src/main/java/com/caishi/capricorn/common/news/dto/NewsDetailInfo.java
@@ -1,4 +1,97 @@
 package com.caishi.capricorn.common.news.dto;
 
+import java.util.List;
+
 public class NewsDetailInfo {
+
+    /**
+     * 新闻ID
+     */
+    private String newsId;
+
+    public String getNewsId() {
+        return newsId;
+    }
+
+    public void setNewsId(String newsId) {
+        this.newsId = newsId;
+    }
+
+    /**
+     * 新闻标题
+     */
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * 详情内容
+     */
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * 图片列表
+     */
+    private List<NewsImageInfo> newsImageInfoList;
+
+    public List<NewsImageInfo> getNewsImageInfoList() {
+        return newsImageInfoList;
+    }
+
+    public void setNewsImageInfoList(List<NewsImageInfo> newsImageInfoList) {
+        this.newsImageInfoList = newsImageInfoList;
+    }
+
+    /**
+     * 分享连接
+     */
+    private String shareUrl;
+
+    public String getShareUrl() {
+        return shareUrl;
+    }
+
+    public void setShareUrl(String shareUrl) {
+        this.shareUrl = shareUrl;
+    }
+
+    /**
+     * 新闻来源
+     */
+    private String source;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    /**
+     * 创建时间
+     */
+    private long createTime;
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
 }

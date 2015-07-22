a5fee931609b0b98827ea58919c4799056ada3f5
James.li <James.li@9icaishi.net>
2015-07-22 10:52:06 +0800

    ....

---
+++ common-data/src/main/java/com/caishi/capricorn/common/data/App.java
@@ -0,0 +1,13 @@
+package com.caishi.capricorn.common.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

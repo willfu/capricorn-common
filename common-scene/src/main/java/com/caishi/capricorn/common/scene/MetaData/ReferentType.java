8ed8636e6eef783add35d2fd4881e39f73e70768
James.li <James.li@9icaishi.net>
2015-07-29 19:03:21 +0800

    add ReferentType

---
+++ common-scene/src/main/java/com/caishi/capricorn/common/scene/MetaData/ReferentType.java
@@ -0,0 +1,29 @@
+package com.caishi.capricorn.common.scene.MetaData;
+
+public enum ReferentType {
+
+    /**
+     * 主Feed流
+     */
+    FEED,
+
+    /**
+     * 主Feed流穿越门
+     */
+    FEED_ACROSS,
+
+    /**
+     * 场景下穿越门
+     */
+    SCENE_ACROSS,
+
+    /**
+     * 足迹
+     */
+    FOOT_MARK,
+
+    /**
+     * html5页面
+     */
+    HTML5;
+}

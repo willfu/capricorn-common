599b3ea517fe4a2c7ee45d1c14683b04a7df23c6
韩小卫 <xwhan1983@163.com>
2015-08-05 12:01:43 +0800

    add constants class to define extend keys for feed message

---
+++ common-base/src/main/java/com/caishi/capricorn/common/base/FeedConstants.java
@@ -0,0 +1,33 @@
+package com.caishi.capricorn.common.base;
+
+/**
+ * This class defines extended constants those used as keys(subkeys) in FeedMessage object
+ *
+ * @author humphrey.han@9icaishi.net
+ * @since 0.0.1
+ */
+public class FeedConstants {
+    /** source link of image **/
+    public static String FEED_MEDIA_IMAGES_SRC = "src";
+
+    /**
+     * source id in debug info
+     */
+    public static String  FEED_EXTRA_DEBUG_INFO_SOURCEID = "sourceId";
+
+    /**
+     * movie score
+     */
+    public static String FEED_EXTRA_MOVIE_SCORE = "movie-score";
+
+    /**
+     * movie actor
+     */
+    public static String FEED_EXTRA_MOVIE_ACTOR = "movie-actor";
+
+    /**
+     * movie release date
+     */
+    public static String FEED_EXTRA_MOVIE_RELEASE_DATE = "movie-releasedate";
+
+}

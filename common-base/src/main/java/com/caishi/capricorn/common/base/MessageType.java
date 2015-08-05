88fb92aa7e401a6cf1afdd9ec33b16504f43e15e
James.li <James.li@9icaishi.net>
2015-08-05 17:15:19 +0800

    add new enum named as messagetype

---
+++ common-base/src/main/java/com/caishi/capricorn/common/base/MessageType.java
@@ -0,0 +1,63 @@
+package com.caishi.capricorn.common.base;
+
+public enum  MessageType {
+    /**
+     * 新闻
+     */
+    NEWS,
+
+    /**
+     * 活动
+     */
+    ACTIVITY,
+
+    /**
+     * 广告
+     */
+    ADVERTISE,
+
+    /**
+     * 段子
+     */
+    JOKES,
+
+    /**
+     * 场景
+     */
+    SCENE,
+
+    /**
+     * 游戏
+     */
+    GAME,
+
+    /**
+     * 图表
+     */
+    CHART,
+
+    /**
+     * 视频
+     */
+    VIDEO,
+
+    /**
+     * 音频
+     */
+    RADIO,
+
+    /**
+     * 测试
+     */
+    QUIZ,
+
+    /**
+     * 投票
+     */
+    VOTE,
+
+    /**
+     * 影视
+     */
+    MOVIE;
+}

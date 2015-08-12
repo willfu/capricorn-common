b240b1c9598dcb10e11516661e141af61356fca7
James <James@AdministratordeMacBook-Pro.local>
2015-08-12 18:57:05 +0800

    make modification to common-log model

--- common-log/src/main/java/com/caishi/capricorn/common/log/dto/news/NewsInteractLogger.java
+++ common-log/src/main/java/com/caishi/capricorn/common/log/dto/news/NewsInteractLogger.java
@@ -2,4 +2,16 @@ package com.caishi.capricorn.common.log.dto.news;
 
 public class NewsInteractLogger extends NewsLogger {
 
+    /**
+     * 交互类型
+     */
+    private String interactType;
+
+    public String getInteractType() {
+        return interactType;
+    }
+
+    public void setInteractType(String interactType) {
+        this.interactType = interactType;
+    }
 }

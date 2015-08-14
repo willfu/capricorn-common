9f5d2e5db178a6f5c3deb9e7ec15d90dc96035ce
James <James@AdministratordeMacBook-Pro.local>
2015-08-14 15:09:51 +0800

    .....

--- common-log/src/main/java/com/caishi/capricorn/common/log/dto/news/NewsLogger.java
+++ common-log/src/main/java/com/caishi/capricorn/common/log/dto/news/NewsLogger.java
@@ -14,30 +14,4 @@ public class NewsLogger {
     public void setNewsId(String newsId) {
         this.newsId = newsId;
     }
-
-    /**
-     * 父本类型
-     */
-    public String parentType;
-
-    public String getParentType() {
-        return parentType;
-    }
-
-    public void setParentType(String parentType) {
-        this.parentType = parentType;
-    }
-
-    /**
-     * 父本ID
-     */
-    private int parentId;
-
-    public int getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(int parentId) {
-        this.parentId = parentId;
-    }
 }

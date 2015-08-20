1d98728e1a2773560e50874f623543306171c03c
叶延超 <alex.ye@9icaishi.net>
2015-08-20 16:12:53 +0800

    add feed message

---
+++ common-base/src/main/java/com/caishi/capricorn/common/base/FeedMessageRefer.java
@@ -0,0 +1,49 @@
+package com.caishi.capricorn.common.base;
+
+import java.io.Serializable;
+
+/**
+ * Created by apple on 15/8/20.
+ * 消息引用数：
+ * 1. commends
+ * 2.page link count
+ */
+public class FeedMessageRefer implements Serializable {
+
+	private String id;
+	private Integer comments;
+	private Integer pageLinkCount;
+
+	public FeedMessageRefer() {
+	}
+
+	public FeedMessageRefer(String id, Integer comments, Integer pageLinkCount) {
+		this.id = id;
+		this.comments = comments;
+		this.pageLinkCount = pageLinkCount;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Integer getComments() {
+		return comments;
+	}
+
+	public void setComments(Integer comments) {
+		this.comments = comments;
+	}
+
+	public Integer getPageLinkCount() {
+		return pageLinkCount;
+	}
+
+	public void setPageLinkCount(Integer pageLinkCount) {
+		this.pageLinkCount = pageLinkCount;
+	}
+}

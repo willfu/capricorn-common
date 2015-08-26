52fc75082b9c2944607292dfb6a662f6284bb940
James <James@AdministratordeMacBook-Pro.local>
2015-08-26 13:51:10 +0800

    add logDto mode into common-log module

---
+++ common-log/src/main/java/com/caishi/capricorn/common/log/dto/LogDto.java
@@ -0,0 +1,72 @@
+package com.caishi.capricorn.common.log.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogDto {
+
+    /**
+     * 日志片段
+     */
+    private String sessionId;
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    /**
+     * 事件名称
+     */
+    private String event;
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    /**
+     * 事件事件
+     */
+    private long time;
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    /**
+     * 作用范围
+     */
+    private String page;
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    /**
+     * 日志参数
+     */
+    private Map<String,Object> param = new HashMap<String, Object>();
+
+    public Map<String, Object> getParam() {
+        return param;
+    }
+
+    public void setParam(Map<String, Object> param) {
+        this.param = param;
+    }
+}

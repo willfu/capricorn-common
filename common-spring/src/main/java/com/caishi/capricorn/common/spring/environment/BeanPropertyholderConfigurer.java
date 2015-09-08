4a6a75abdcb2dd9f81e980cd9f69b0499aef4e1a
James <James@AdministratordeMacBook-Pro.local>
2015-09-08 13:48:54 +0800

    add @value feature for properties file in contect

--- common-spring/src/main/java/com/caishi/capricorn/common/spring/environment/BeanPropertyholderConfigurer.java
+++ common-spring/src/main/java/com/caishi/capricorn/common/spring/environment/BeanPropertyholderConfigurer.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Sets;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.core.io.Resource;
@@ -17,129 +18,156 @@ import java.util.*;
 /**
  * 可以按照不同的运行模式启用相应的配置
  */
-public class BeanPropertyholderConfigurer extends PropertyPlaceholderConfigurer implements InitializingBean {
+public class BeanPropertyholderConfigurer extends PropertyPlaceholderConfigurer implements FactoryBean<Properties>, InitializingBean {
 
-	public static final String ONLINE = "ONLINE";
-	public static final String TEST = "TEST";
-	public static final String DEV = "DEV";
-	public static final Set<String> support_modes = Sets.newHashSet(ONLINE, TEST, DEV);
-	public static final String SEPARATOR = "_";
-	public static final String PRODUCTION_MODE = "production.mode";
-	private static final Logger logger = LoggerFactory.getLogger(BeanPropertyholderConfigurer.class);
-	/**
-	 * working directory config : production mode
-	 */
-	private static final String WORK_DIR_CONFIG = System.getProperty("user.dir") + "/config/production.properties";
-	/**
-	 * classpath directory config : production mode
-	 */
-	private static final String CLASSPATH_DIR_CONFIG = "config/production.properties";
+    public static final String ONLINE = "ONLINE";
+    public static final String TEST = "TEST";
+    public static final String DEV = "DEV";
+    public static final Set<String> support_modes = Sets.newHashSet(ONLINE, TEST, DEV);
+    public static final String SEPARATOR = "_";
+    public static final String PRODUCTION_MODE = "production.mode";
+    private static final Logger logger = LoggerFactory.getLogger(BeanPropertyholderConfigurer.class);
+    /**
+     * working directory config : production mode
+     */
+    private static final String WORK_DIR_CONFIG = System.getProperty("user.dir") + "/config/production.properties";
+    /**
+     * classpath directory config : production mode
+     */
+    private static final String CLASSPATH_DIR_CONFIG = "config/production.properties";
 
-	//设置运行程序的模式，默认是开发模式，当进入测试或者发布上线就修改为其他模式
-	private String mode = DEV;
-	//缓存所有的属性配置
-	private Properties properties;
-	//配置文件存放路径
-	private String pathPattern = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX
-			+ "/config/%s/*.properties";
+    private boolean singleton = true;
 
-	/**
-	 * @return the path
-	 */
-	public String getPathPattern() {
-		return pathPattern;
-	}
+    //设置运行程序的模式，默认是开发模式，当进入测试或者发布上线就修改为其他模式
+    private String mode = DEV;
+    //缓存所有的属性配置
+    private Properties properties;
+    //配置文件存放路径
+    private String pathPattern = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX
+            + "/config/%s/*.properties";
 
-	/**
-	 * @param pathPattern the path to set
-	 */
-	public void setPathPattern(String pathPattern) {
-		this.pathPattern = pathPattern;
-	}
+    /**
+     * @return the path
+     */
+    public String getPathPattern() {
+        return pathPattern;
+    }
 
-	/**
-	 * @return the mode
-	 */
-	public String getMode() {
-		return mode;
-	}
+    /**
+     * @param pathPattern the path to set
+     */
+    public void setPathPattern(String pathPattern) {
+        this.pathPattern = pathPattern;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see
-	 * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
-	 */
-	public void afterPropertiesSet() throws Exception {
-		PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
-		try {
+    /**
+     * @return the mode
+     */
+    public String getMode() {
+        return mode;
+    }
 
-			mode = detectMode();
+    /*
+     * (non-Javadoc)
+     * @see
+     * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+     */
+    public void afterPropertiesSet() throws Exception {
+        if (this.singleton) {
+            this.properties = createProperties();
+        }
+    }
 
-			pathPattern = generatePath(pathPattern, mode);
-			Resource[] userResources = resourcePatternResolver.getResources(pathPattern);
+    private Properties createProperties() {
+        PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        try {
 
-			List<Resource> resourceList = new ArrayList<Resource>();
-			if (userResources != null) {
-				resourceList.addAll(Arrays.asList(userResources));
-			}
+            mode = detectMode();
 
-			super.setLocations(resourceList.toArray(new Resource[]{}));
+            pathPattern = generatePath(pathPattern, mode);
+            Resource[] userResources = resourcePatternResolver.getResources(pathPattern);
 
-			Properties result = this.mergeProperties();
-			this.properties = result;
-			if (logger.isDebugEnabled()) {
-				logger.debug("production model is " + mode);
-			}
-		} catch (IOException e) {
-			logger.error("load resource error ", e);
-			throw new RuntimeException(e);
-		}
-	}
+            List<Resource> resourceList = new ArrayList<Resource>();
+            if (userResources != null) {
+                resourceList.addAll(Arrays.asList(userResources));
+            }
 
-	private String detectMode() throws IOException {
-		//系统属性优先
-		String mode = System.getProperty(PRODUCTION_MODE);
+            super.setLocations(resourceList.toArray(new Resource[]{}));
 
-		if (StringUtils.isNotBlank(mode) && support_modes.contains(mode)) {
-			logger.debug("loading production config for ");
-			return mode;
-		}
+            Properties result = this.mergeProperties();
+            if (logger.isDebugEnabled()) {
+                logger.debug("production model is " + mode);
+            }
+            return result;
+        } catch (IOException e) {
+            logger.error("load resource error ", e);
+            throw new RuntimeException(e);
+        }
+    }
 
-		//work directory config
-		Properties workConfigProperties = PropertiesLoaderUtils.loadAllProperties(WORK_DIR_CONFIG);
-		mode = workConfigProperties.getProperty(PRODUCTION_MODE);
-		if (StringUtils.isNotBlank(mode) && support_modes.contains(mode)) {
-			return mode;
-		}
+    private String detectMode() throws IOException {
+        //系统属性优先
+        String mode = System.getProperty(PRODUCTION_MODE);
 
-		// class path config
-		Properties classpathProperties = PropertiesLoaderUtils.loadAllProperties(CLASSPATH_DIR_CONFIG);
-		mode = classpathProperties.getProperty(PRODUCTION_MODE);
-		if (StringUtils.isNotBlank(mode) && support_modes.contains(mode)) {
-			return mode;
-		}
+        if (StringUtils.isNotBlank(mode) && support_modes.contains(mode)) {
+            logger.debug("loading production config for ");
+            return mode;
+        }
 
-		if (!support_modes.contains(mode)) {
-			mode = DEV;
-		}
-		return mode;
-	}
+        //work directory config
+        Properties workConfigProperties = PropertiesLoaderUtils.loadAllProperties(WORK_DIR_CONFIG);
+        mode = workConfigProperties.getProperty(PRODUCTION_MODE);
+        if (StringUtils.isNotBlank(mode) && support_modes.contains(mode)) {
+            return mode;
+        }
+
+        // class path config
+        Properties classpathProperties = PropertiesLoaderUtils.loadAllProperties(CLASSPATH_DIR_CONFIG);
+        mode = classpathProperties.getProperty(PRODUCTION_MODE);
+        if (StringUtils.isNotBlank(mode) && support_modes.contains(mode)) {
+            return mode;
+        }
+
+        if (!support_modes.contains(mode)) {
+            mode = DEV;
+        }
+        return mode;
+    }
 
 
-	private String generatePath(String pathPattern, String mode) {
-		if (StringUtils.equals(mode, ONLINE)) {
-			return String.format(pathPattern, StringUtils.lowerCase(mode));
-		}
-		if (StringUtils.equals(mode, TEST)) {
-			return String.format(pathPattern, StringUtils.lowerCase(mode));
-		}
-		if (StringUtils.equals(mode, DEV)) {
-			return String.format(pathPattern, StringUtils.lowerCase(mode));
-		}
-		return String.format(pathPattern, StringUtils.lowerCase(DEV));
-	}
+    private String generatePath(String pathPattern, String mode) {
+        if (StringUtils.equals(mode, ONLINE)) {
+            return String.format(pathPattern, StringUtils.lowerCase(mode));
+        }
+        if (StringUtils.equals(mode, TEST)) {
+            return String.format(pathPattern, StringUtils.lowerCase(mode));
+        }
+        if (StringUtils.equals(mode, DEV)) {
+            return String.format(pathPattern, StringUtils.lowerCase(mode));
+        }
+        return String.format(pathPattern, StringUtils.lowerCase(DEV));
+    }
 
-	public String getProperty(String key) {
-		return resolvePlaceholder(key, properties);
-	}
+    public String getProperty(String key) {
+        return resolvePlaceholder(key, properties);
+    }
+
+    @Override
+    public Properties getObject() throws Exception {
+        if (this.singleton) {
+            return this.properties;
+        } else {
+            return createProperties();
+        }
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return Properties.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return this.singleton;
+    }
 }

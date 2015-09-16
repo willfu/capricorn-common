c2a0e816da9fb03a2ccc04f37b1c4d2cf677c827
叶延超 <alex.ye@9icaishi.net>
2015-09-16 14:57:22 +0800

    add java message formatter

---
+++ common-kafka/src/main/java/com/caishi/capricorn/common/kafka/utils/JavaMsgFormatter.java
@@ -0,0 +1,40 @@
+package com.caishi.capricorn.common.kafka.utils;
+
+import com.alibaba.fastjson.JSON;
+import kafka.tools.MessageFormatter;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintStream;
+import java.util.Properties;
+
+/**
+ * Created by apple on 15/9/16.
+ */
+public class JavaMsgFormatter implements MessageFormatter {
+	@Override
+	public void writeTo(byte[] bytes, byte[] bytes1, PrintStream printStream) {
+		if (bytes1 != null) {
+			try {
+				ObjectInputStream oin = new ObjectInputStream(new ByteArrayInputStream(bytes1));
+				Object object = oin.readObject();
+				printStream.write(JSON.toJSONBytes(object));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void init(Properties properties) {
+
+	}
+
+	@Override
+	public void close() {
+
+	}
+}

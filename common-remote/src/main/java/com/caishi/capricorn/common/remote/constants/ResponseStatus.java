7494a20d89813988bdfd521cd39fe10768181900
James <James@AdministratordeMacBook-Pro.local>
2015-10-12 14:13:13 +0800

    add responsestatucs class into module

---
+++ common-remote/src/main/java/com/caishi/capricorn/common/remote/constants/ResponseStatus.java
@@ -0,0 +1,8 @@
+package com.caishi.capricorn.common.remote.constants;
+
+public class ResponseStatus {
+
+    public final static int CONNECT_SUCCESS = 200;
+
+    public final static int RESPONSE_SUCCESS = 10000;
+}

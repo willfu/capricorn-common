322c8e2c100ff100aaaf3856b4984bbd7eedca01
James <James@AdministratordeMacBook-Pro.local>
2015-10-12 11:07:22 +0800

    add serviceResult into module

---
+++ common-remote/src/main/java/com/caishi/capricorn/common/remote/result/ServiceResult.java
@@ -0,0 +1,78 @@
+package com.caishi.capricorn.common.remote.result;
+
+import com.caishi.capricorn.common.remote.constants.OperationStatus;
+import com.caishi.capricorn.common.remote.model.OperationResult;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+public class ServiceResult<T> implements Serializable {
+
+    /**
+     *
+     */
+    private String requestId = UUID.randomUUID().toString();
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+
+    /**
+     *
+     */
+    private long requestStartTime = new Date().getTime();
+
+    public long getRequestStartTime() {
+        return requestStartTime;
+    }
+
+    public void setRequestStartTime(long requestStartTime) {
+        this.requestStartTime = requestStartTime;
+    }
+
+    /**
+     *
+     */
+    private long requestEndTime;
+
+    public long getRequestEndTime() {
+        return requestEndTime;
+    }
+
+    public void setRequestEndTime(long requestEndTime) {
+        this.requestEndTime = requestEndTime;
+    }
+
+    /**
+     *
+     */
+    private OperationResult operationResult = new OperationResult(OperationStatus.SUCC,null);
+
+    public OperationResult getOperationResult() {
+        return operationResult;
+    }
+
+    public void setOperationResult(OperationResult operationResult) {
+        this.operationResult = operationResult;
+    }
+
+    /**
+     *
+     */
+    private T result = null;
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        setRequestEndTime(new Date().getTime());
+        this.result = result;
+    }
+}

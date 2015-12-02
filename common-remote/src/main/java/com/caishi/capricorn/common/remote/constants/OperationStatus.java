7aae95e6d05b7f3d385413e84fc6f38b0e7e0919
韩小卫 <xwhan1983@163.com>
2015-12-02 10:48:10 +0800

    Add help methods for ServiceResult

--- common-remote/src/main/java/com/caishi/capricorn/common/remote/constants/OperationStatus.java
+++ common-remote/src/main/java/com/caishi/capricorn/common/remote/constants/OperationStatus.java
@@ -8,7 +8,14 @@ import java.io.Serializable;
 public enum OperationStatus implements Serializable {
 
 	SUCC("succeed", "operation is successed"), FAILED("failed", "operation is failed"),
-	ERROR("error", "errors occur in operation");
+	ERROR("error", "errors occur in operation"),
+	BAD_REQUEST("bad_request", "request is invalid or input data is invalid"),
+	UNAUTHORIZED("unauthorized", "operation is unauthorized"),
+	FORBIDDEN("forbidden", "operation is forbidden"),
+	NOT_FOUND("not_found", "result not found"),
+	NOT_SUPPORTED("not_supported", "operation is not supported"),
+	UNAVAILABLE("unavailable", "operation is unavailable"),
+	TIMEOUT("timeout", "operation timed out");
 
 	private String status;
 
--- common-remote/src/main/java/com/caishi/capricorn/common/remote/result/ServiceResult.java
+++ common-remote/src/main/java/com/caishi/capricorn/common/remote/result/ServiceResult.java
@@ -3,7 +3,9 @@ package com.caishi.capricorn.common.remote.result;
 import com.caishi.capricorn.common.remote.constants.OperationStatus;
 import com.caishi.capricorn.common.remote.model.OperationResult;
 
+import java.io.PrintWriter;
 import java.io.Serializable;
+import java.io.StringWriter;
 import java.util.Date;
 import java.util.UUID;
 
@@ -67,6 +69,8 @@ public class ServiceResult<T> implements Serializable {
      */
     private T result = null;
 
+    private Exception exception;
+
     public T getResult() {
         return result;
     }
@@ -74,5 +78,83 @@ public class ServiceResult<T> implements Serializable {
     public void setResult(T result) {
         setRequestEndTime(new Date().getTime());
         this.result = result;
+    }
+
+    /**
+     * 接口调用成功，不需要返回对象
+     */
+    public static <T> ServiceResult<T> newSuccess(){
+        ServiceResult<T> result = new ServiceResult<>();
+        return result;
+    }
+
+    /**
+     * 接口调用成功，有返回对象
+     */
+    public static <T> ServiceResult<T> newSuccess(T object) {
+        ServiceResult<T> result = new ServiceResult<>();
+        result.setResult(object);
+        return result;
+    }
+
+    /**
+     * 接口调用失败，有错误码和描述，没有返回对象
+     * 注：由于Operation
+     */
+    public static <T> ServiceResult<T> newFailure(OperationStatus status, String message){
+        ServiceResult<T> result = new ServiceResult<>();
+        result.setOperationResult(new OperationResult(status, message));
+        return result;
+    }
+
+    /**
+     * 接口调用失败，返回异常信息
+     */
+    public static <T> ServiceResult<T> newException(Exception e){
+        ServiceResult<T> result = new ServiceResult<>();
+        result.setOperationResult(new OperationResult(OperationStatus.ERROR, "Internal Server Error"));
+        result.setException(e);
+        return result;
+    }
+
+    /** 判断返回结果是否成功 */
+    public boolean success() {
+        return OperationStatus.SUCC == this.operationResult.getStatus();
+    }
+    /** 判断返回结果是否有结果对象 */
+    public boolean hasObject() {
+        return OperationStatus.SUCC == this.operationResult.getStatus() && result != null;
+    }
+    /** 判断返回结果是否有异常 */
+    public boolean hasException() {
+        return exception != null;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+
+    public String toString() {
+        StringBuilder serviceResult = new StringBuilder("ServiceResult");
+        if(result != null) {
+            serviceResult.append("<" + result.getClass().getSimpleName() + ">");
+        }
+
+        serviceResult.append(": {code=" + operationResult.getStatus().getStatus());
+        if(result != null) {
+            serviceResult.append(", object=" + result);
+        }
+
+        if(operationResult.getMsg() != null) {
+            serviceResult.append(", message=" + operationResult.getMsg());
+        }
+
+        if(exception!=null) {
+            StringWriter stringWriter = new StringWriter();
+            exception.printStackTrace(new PrintWriter(stringWriter));
+            serviceResult.append(", exception=" + stringWriter.toString());
+        }
+        serviceResult.append(" }");
+        return serviceResult.toString();
     }
 }

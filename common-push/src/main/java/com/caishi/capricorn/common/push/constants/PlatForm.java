3175acc2208b7524432ece3e75c016b5b3fc0c11
james.li <james.li@9icaishi.net>
2015-12-10 12:09:37 +0800

    fix bugs

--- common-push/src/main/java/com/caishi/capricorn/common/push/constants/PlatForm.java
+++ common-push/src/main/java/com/caishi/capricorn/common/push/constants/PlatForm.java
@@ -43,11 +43,11 @@ public enum PlatForm {
      */
     private String name;
 
-    private String getName() {
+    public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
 
@@ -56,11 +56,11 @@ public enum PlatForm {
      */
     private String description;
 
-    private String getDescription() {
+    public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    private void setDescription(String description) {
         this.description = description;
     }
 

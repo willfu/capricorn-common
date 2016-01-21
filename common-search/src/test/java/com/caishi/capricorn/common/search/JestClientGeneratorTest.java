af8c49b79f872a760047556ca35b5cd1e1f74be7
james.li <james.li@9icaishi.net>
2016-01-21 11:56:34 +0800

    fix bugs

--- common-search/src/test/java/com/caishi/capricorn/common/search/JestClientGeneratorTest.java
+++ common-search/src/test/java/com/caishi/capricorn/common/search/JestClientGeneratorTest.java
@@ -1,6 +1,7 @@
 package com.caishi.capricorn.common.search;
 
 import com.caishi.capricorn.common.search.client.JestClientGenerator;
+import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import junit.framework.TestCase;
 import org.junit.Assert;
@@ -12,5 +13,7 @@ public class JestClientGeneratorTest extends TestCase {
         JestClientGenerator jestClientGenerator = new JestClientGenerator("http://10.1.1.121:9200");
         JestClientFactory jestClientFactory = jestClientGenerator.getJestClientFactory();
         Assert.assertNotNull(jestClientFactory);
+        JestClient jestClient = jestClientGenerator.getJestClient();
+        Assert.assertNotNull(jestClient);
     }
 }
